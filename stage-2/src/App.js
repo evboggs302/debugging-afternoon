990120a7b2ef619b8c472f36689c7465b9d4b93a
Evan Boggs Jr <evanboggs@Evans-MacBook-Pro.local>
2019-05-14 13:51:30 -0700

    stage 2 complete

--- stage-2/src/App.js
+++ stage-2/src/App.js
@@ -5,7 +5,7 @@ import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
 import NavBar from "./Components/NavBar/NavBar";
 
 class App extends Component {
-  constuctor(props) {
+  constructor(props) {
     super(props);
     this.state = {
       products: [],
@@ -21,8 +21,9 @@ class App extends Component {
       .get("https://practiceapi.devmountain.com/products/")
       .then(response => {
         this.setState({
-          products: response
+          products: response.data
         });
+        console.log(this.state.products);
       });
   }
   addToCart(item) {
